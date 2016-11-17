1117f1b25dcdfca3e59853fd44eea886c438d91a
GiantTree <webmaster@gianttree.de>
2016-11-17 20:35:01 +0100

    Catch newly created databases missing the version information

--- src/kotlin/gtlp/groundmc/lobby/database/table/Meta.kt
+++ src/kotlin/gtlp/groundmc/lobby/database/table/Meta.kt
@@ -1,7 +1,11 @@
 package gtlp.groundmc.lobby.database.table
 
+import gtlp.groundmc.lobby.LobbyMain
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.*
 
 object Meta : Table() {
     private val CURRENT_TABLE_VER = 1
@@ -9,11 +13,21 @@ object Meta : Table() {
     private val version = integer("version").default(CURRENT_TABLE_VER).uniqueIndex().primaryKey()
 
     fun upgradeDatabase() {
-        val currentVersion = Meta.selectAll().first()[version]
-        for (version in currentVersion..CURRENT_TABLE_VER) {
-            when (version) {
+        try {
+            transaction {
+                val currentVersion = Meta.selectAll().first()[version]
+                for (version in currentVersion..CURRENT_TABLE_VER) {
+                    when (version) {
 
+                    }
+                }
             }
+        } catch (exception: NoSuchElementException) {
+            transaction {
+                Meta.insert { it[version] = CURRENT_TABLE_VER }
+                commit()
+            }
+            LobbyMain.instance?.logger?.info("Database newly created, no update necessary")
         }
     }
 }
