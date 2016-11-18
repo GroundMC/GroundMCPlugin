d395485f3b62866d58fe7d47ed6a2c46351148fe
GiantTree <webmaster@gianttree.de>
2016-11-19 00:56:27 +0100

    Set hard values for maximum life and saturation, removes useless effect.

--- src/kotlin/gtlp/groundmc/lobby/task/ApplyPlayerEffectsTask.kt
+++ src/kotlin/gtlp/groundmc/lobby/task/ApplyPlayerEffectsTask.kt
@@ -2,8 +2,6 @@ package gtlp.groundmc.lobby.task
 
 import gtlp.groundmc.lobby.LobbyMain
 import org.bukkit.Bukkit
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
 
 object ApplyPlayerEffectsTask : ITask {
     override val delay: Long = 20L
@@ -11,8 +9,8 @@ object ApplyPlayerEffectsTask : ITask {
 
     override fun run() {
         Bukkit.getServer().onlinePlayers.filter { it.world == LobbyMain.hubWorld }.forEach {
-            it.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION, 200, 1, false, false), true)
-            it.addPotionEffect(PotionEffect(PotionEffectType.SATURATION, 200, 1, false, false), true)
+            it.health = 20.0
+            it.saturation = 20.0f
         }
     }
 }
