8a00ab2a02306dd6e2c536cc7ed50eff3f71e535
GiantTree <webmaster@gianttree.de>
2016-12-21 16:20:51 +0100

    Add column to track the last date a player got daily coins

--- src/kotlin/gtlp/groundmc/lobby/database/table/Users.kt
+++ src/kotlin/gtlp/groundmc/lobby/database/table/Users.kt
@@ -2,6 +2,7 @@ package gtlp.groundmc.lobby.database.table
 
 import gtlp.groundmc.lobby.enum.VisibilityStates
 import org.jetbrains.exposed.sql.Table
+import org.joda.time.DateTime
 
 /**
  * Table to hold players' settings and friends
@@ -12,4 +13,5 @@ object Users : Table() {
     val hiddenStatus = enumeration("hidden_status", VisibilityStates::class.java).default(VisibilityStates.ALL)
     val vanishStatus = bool("vanish_status").default(false)
     val coins = long("coins").default(0)
+    val lastDailyCoinsDate = date("lastDailyCoins").default(DateTime.parse("1970-01-01"))
 }
