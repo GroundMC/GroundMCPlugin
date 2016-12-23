a58f1f79ce01bfc8f7fd9dab2b256739b062a7a3
GiantTree <webmaster@gianttree.de>
2016-12-23 15:32:11 +0100

    Add i18nKey to RelationshipLevel.kt. Allows for easy translation.

--- src/kotlin/gtlp/groundmc/lobby/enum/RelationshipLevel.kt
+++ src/kotlin/gtlp/groundmc/lobby/enum/RelationshipLevel.kt
@@ -3,10 +3,10 @@ package gtlp.groundmc.lobby.enum
 /**
  * Enumeration of levels of relationships
  */
-enum class RelationshipLevel(val limit: Int) {
-    KNOWN(Int.MAX_VALUE),
-    WELL_KNOWN(Int.MAX_VALUE),
-    FRIEND(100),
-    GOOD_FRIEND(20),
-    BEST_FRIEND(5);
+enum class RelationshipLevel(val limit: Int, val i18nKey: String) {
+    KNOWN(Int.MAX_VALUE, "relationship.known"),
+    WELL_KNOWN(Int.MAX_VALUE, "relationship.well_known"),
+    FRIEND(100, "relationship.friend"),
+    GOOD_FRIEND(20, "relationship.good_friend"),
+    BEST_FRIEND(5, "relationship.best_friend");
 }
