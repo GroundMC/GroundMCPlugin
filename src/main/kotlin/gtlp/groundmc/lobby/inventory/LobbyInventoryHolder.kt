782a57ad9604468e2d686779d2b92fa4535983c7
GiantTree <webmaster@gianttree.de>
2017-04-22 13:43:40 +0200

    Store and restore the non-hub inventory.

--- src/main/kotlin/gtlp/groundmc/lobby/inventory/LobbyInventoryHolder.kt
+++ src/main/kotlin/gtlp/groundmc/lobby/inventory/LobbyInventoryHolder.kt
@@ -4,14 +4,13 @@ import gtlp.groundmc.lobby.LobbyMain
 import gtlp.groundmc.lobby.util.entering
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 /**
  * Holds the two inventories used by our plugin for a player.
  */
-class LobbyInventoryHolder private constructor(inventory1: Inventory, inventory2: Inventory) {
-    var lobbyInventory = inventory1
-    var hidePlayerInventory = inventory2
-
+class LobbyInventoryHolder private constructor(var lobbyInventory: Inventory, var hidePlayerInventory: Inventory) {
+    var originalContents: Array<ItemStack>? = null
 
     companion object {
         fun forPlayer(player: Player): LobbyInventoryHolder {
