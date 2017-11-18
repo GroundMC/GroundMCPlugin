5203d9e974c23d30828c70c623fa69886948eddf
GiantTree <webmaster@gianttree.de>
2017-11-18 22:50:27 +0100

    Use `Permission` objects instead of strings

--- src/main/kotlin/gtlp/groundmc/lobby/enums/Permission.kt
+++ src/main/kotlin/gtlp/groundmc/lobby/enums/Permission.kt
@@ -1,35 +1,37 @@
 package gtlp.groundmc.lobby.enums
 
+import org.bukkit.Bukkit
+
 /**
  * Enum to store permissions as strings.
  * Consolidates all permissions into one enum.
  */
 enum class Permission constructor(
         /**
-         * The string representation of this permission.
+         * The permission object used by Bukkit.
          * Can be used in plugins that manage permissions.
          */
-        val id: String
+        val permission: org.bukkit.permissions.Permission
 ) {
     /**
      * The administrator permission.
      * Players with this permission usually have full control over the functionality
      * of the plugin.
      */
-    ADMIN("groundmc.lobby.admin"),
+    ADMIN(Bukkit.getServer().pluginManager.getPermission("groundmc.lobby.admin")),
 
     /**
      * Players with this permission are allowed to hide other players from their view.
      */
-    HIDE_PLAYERS("groundmc.lobby.hide_players"),
+    HIDE_PLAYERS(Bukkit.getServer().pluginManager.getPermission("groundmc.lobby.hide_players")),
 
     /**
      * Players with this permission are allowed to silence their chat.
      */
-    SILENT("groundmc.lobby.silent"),
+    SILENT(Bukkit.getServer().pluginManager.getPermission("groundmc.lobby.silent")),
 
     /**
      * Players with this permission are allowed to vanish and become invisible to other players.
      */
-    VANISH("groundmc.lobby.vanish");
+    VANISH(Bukkit.getServer().pluginManager.getPermission("groundmc.lobby.vanish"));
 }
