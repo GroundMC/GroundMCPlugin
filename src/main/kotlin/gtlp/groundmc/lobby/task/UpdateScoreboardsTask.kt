b1820338ed7f9b4d12ab3956569b123d41e4e4e6
GiantTree <marv.1997@live.de>
2017-12-09 17:51:33 +0100

    Create the scoreboard objectives dynamically.
    Signed-off-by: GiantTree <marv.1997@live.de>

--- src/main/kotlin/gtlp/groundmc/lobby/task/UpdateScoreboardsTask.kt
+++ src/main/kotlin/gtlp/groundmc/lobby/task/UpdateScoreboardsTask.kt
@@ -1,5 +1,7 @@
 package gtlp.groundmc.lobby.task
 
+import gtlp.groundmc.lobby.database.table.Events
+import gtlp.groundmc.lobby.database.table.Users
 import org.apache.commons.lang.time.DurationFormatUtils
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
@@ -16,13 +18,23 @@ object UpdateScoreboardsTask : ITask {
             scoreboard.registerNewObjective("1", "dummy").apply {
                 displaySlot = DisplaySlot.SIDEBAR
                 displayName = "${ChatColor.GRAY}${it.displayName}"
-                getScore("${ChatColor.GOLD}Spielzeit").score = 5
-                getScore(ChatColor.WHITE.toString() + DurationFormatUtils.formatDuration(
+
+                val lines = mutableListOf<String>()
+                lines += "${ChatColor.GOLD}Spielzeit"
+                lines += ChatColor.WHITE.toString() + DurationFormatUtils.formatDuration(
                         it.getStatistic(Statistic.PLAY_ONE_TICK) * 50L,
-                        "HH:mm")).score = 4
-                getScore("${ChatColor.WHITE}Coming Soon!").score = 3
-                getScore("${ChatColor.DARK_AQUA}Event!").score = 2
-                getScore("${ChatColor.GREEN}JA!").score = 1
+                        "HH:mm")
+                lines += ChatColor.YELLOW.toString() + Users.getPlayer(it)[Users.coins]
+                lines += "${ChatColor.WHITE}Coming Soon!"
+                lines += "${ChatColor.DARK_AQUA}Events!"
+                lines += "${ChatColor.GREEN}JA!"
+                Events.getCurrentEventTitles().forEach { lines += it }
+
+                var score = 0
+                lines.asReversed().forEach {
+                    getScore(it).score = score++
+                }
+
             }
             it.scoreboard = scoreboard
         }
