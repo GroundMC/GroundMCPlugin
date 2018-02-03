db47c869d9188f4b3e014120c2115f5929509745
GiantTree <marv.1997@live.de>
2018-02-03 15:27:28 +0100

    Try to use the PlayerExecutorBridge again.

--- src/main/kotlin/gtlp/groundmc/lobby/event/listener/LobbyChooserListener.kt
+++ src/main/kotlin/gtlp/groundmc/lobby/event/listener/LobbyChooserListener.kt
@@ -1,14 +1,13 @@
 package gtlp.groundmc.lobby.event.listener
 
-import com.google.common.io.ByteStreams
+import de.dytanic.cloudnet.api.CloudAPI
+import de.dytanic.cloudnet.api.player.PlayerExecutorBridge
 import gtlp.groundmc.lobby.Items
-import gtlp.groundmc.lobby.LobbyMain
 import gtlp.groundmc.lobby.enums.GMCType
 import gtlp.groundmc.lobby.enums.NBTIdentifier
 import gtlp.groundmc.lobby.inventory.LobbyChooser
 import gtlp.groundmc.lobby.inventory.LobbyInventory
 import gtlp.groundmc.lobby.util.NBTItemExt
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
@@ -16,7 +15,6 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.plugin.Plugin
 
 
 @Suppress("unused")
@@ -53,19 +51,15 @@ object LobbyChooserListener : Listener {
             val nbtItem = NBTItemExt(event.currentItem)
             if (NBTIdentifier.itemHasPrefix(nbtItem.item) &&
                     nbtItem.getInteger(NBTIdentifier.TYPE) == GMCType.CHOOSE_LOBBY.ordinal) {
-                sendPlayerToServer(LobbyMain.instance, player,
+                sendPlayerToServer(player,
                         nbtItem.getString(NBTIdentifier.TP_LOC)!!)
             }
         }
     }
 
-    private fun sendPlayerToServer(plugin: Plugin, player: Player, server: String) {
-        if (!Bukkit.getMessenger().isOutgoingChannelRegistered(plugin, channel)) {
-            Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, channel)
-        }
-        val byteArrayDataOutput = ByteStreams.newDataOutput()
-        byteArrayDataOutput.writeUTF("Connect")
-        byteArrayDataOutput.writeUTF(server)
-        player.sendPluginMessage(plugin, channel, byteArrayDataOutput.toByteArray())
+    private fun sendPlayerToServer(player: Player, server: String) {
+        PlayerExecutorBridge().sendPlayer(
+                CloudAPI.getInstance().getOnlinePlayer(player.uniqueId),
+                server)
     }
 }
