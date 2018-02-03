95e678464f4dd4ad7fbfb9056dd87be6552eb221
GiantTree <marv.1997@live.de>
2018-02-03 15:03:52 +0100

    Switch to custom plugin channel because the PlayerExecutor is broken af.

--- src/main/kotlin/gtlp/groundmc/lobby/event/listener/LobbyChooserListener.kt
+++ src/main/kotlin/gtlp/groundmc/lobby/event/listener/LobbyChooserListener.kt
@@ -1,13 +1,14 @@
 package gtlp.groundmc.lobby.event.listener
 
-import de.dytanic.cloudnet.api.CloudAPI
-import de.dytanic.cloudnet.api.player.PlayerExecutorBridge
+import com.google.common.io.ByteStreams
 import gtlp.groundmc.lobby.Items
+import gtlp.groundmc.lobby.LobbyMain
 import gtlp.groundmc.lobby.enums.GMCType
 import gtlp.groundmc.lobby.enums.NBTIdentifier
 import gtlp.groundmc.lobby.inventory.LobbyChooser
 import gtlp.groundmc.lobby.inventory.LobbyInventory
 import gtlp.groundmc.lobby.util.NBTItemExt
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
@@ -15,11 +16,13 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.plugin.Plugin
+
 
 @Suppress("unused")
 object LobbyChooserListener : Listener {
 
-    val playerExecutorBridge = PlayerExecutorBridge()
+    private const val channel = "CloudNet"
 
     @EventHandler
     fun openLobbyChooser(event: InventoryClickEvent) {
@@ -50,10 +53,19 @@ object LobbyChooserListener : Listener {
             val nbtItem = NBTItemExt(event.currentItem)
             if (NBTIdentifier.itemHasPrefix(nbtItem.item) &&
                     nbtItem.getInteger(NBTIdentifier.TYPE) == GMCType.CHOOSE_LOBBY.ordinal) {
-                playerExecutorBridge.sendPlayer(
-                        CloudAPI.getInstance().getOnlinePlayer(player.uniqueId),
-                        nbtItem.getString(NBTIdentifier.TP_LOC))
+                sendPlayerToServer(LobbyMain.instance, player,
+                        nbtItem.getString(NBTIdentifier.TP_LOC)!!)
             }
         }
+    }
+
+    private fun sendPlayerToServer(plugin: Plugin, player: Player, server: String) {
+        if (!Bukkit.getMessenger().isOutgoingChannelRegistered(plugin, channel)) {
+            Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, channel)
+        }
+        val byteArrayDataOutput = ByteStreams.newDataOutput()
+        byteArrayDataOutput.writeUTF("Connect")
+        byteArrayDataOutput.writeUTF(server)
+        player.sendPluginMessage(plugin, channel, byteArrayDataOutput.toByteArray())
     }
 }
