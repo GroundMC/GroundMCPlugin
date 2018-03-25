7649e4a84fb36af5f2a16c1ce394cf477a9f407e
GiantTree <marv.1997@live.de>
2018-03-25 23:26:17 +0200

    Add german language test

--- src/test/kotlin/gtlp/groundmc/lobby/util/I18NStringsTest.kt
+++ src/test/kotlin/gtlp/groundmc/lobby/util/I18NStringsTest.kt
@@ -3,13 +3,22 @@ package gtlp.groundmc.lobby.util
 import org.junit.jupiter.api.Assertions.assertNotNull
 import java.util.*
 import kotlin.test.Test
+import kotlin.test.assertNotEquals
 
 class I18NStringsTest {
 
     @Test
-    fun get() {
+    fun getEnglish() {
         I18NStrings.values().forEach {
             assertNotNull(it.get(Locale.US))
+        }
+    }
+
+    @Test
+    fun getGerman() {
+        I18NStrings.values().forEach {
+            assertNotNull(it.get(Locale.GERMAN))
+            assertNotEquals(it.get(Locale.ROOT), it.get(Locale.GERMAN))
         }
     }
 }
