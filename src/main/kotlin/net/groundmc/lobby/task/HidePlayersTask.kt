45df8445a871167ab6559340a6091abe3e77ac23
GiantTree <marv.1997@live.de>
2018-05-06 14:39:30 +0200

    Remove unused transaction

--- src/main/kotlin/net/groundmc/lobby/task/HidePlayersTask.kt
+++ src/main/kotlin/net/groundmc/lobby/task/HidePlayersTask.kt
@@ -5,7 +5,6 @@ import net.groundmc.lobby.database.table.Relationships
 import net.groundmc.lobby.database.table.Users
 import net.groundmc.lobby.enums.VisibilityStates
 import org.bukkit.Bukkit
-import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * Task to update players and their change in the visibility state.
@@ -15,29 +14,27 @@ object HidePlayersTask : ITask {
     override val period: Long = 40L
 
     override fun run() {
-        transaction {
-            val players = Bukkit.getOnlinePlayers()
-            val onlinePlayers =
-                    Users.getAll(players.map { it.uniqueId })
-                            .mapKeys { Bukkit.getPlayer(it.key) }
-            onlinePlayers.forEach { player, status ->
-                when (status[Users.vanishStatus]) {
-                    true -> players.forEach {
-                        it.hidePlayer(LobbyMain.instance, player)
+        val players = Bukkit.getOnlinePlayers()
+        val onlinePlayers =
+                Users.getAll(players.map { it.uniqueId })
+                        .mapKeys { Bukkit.getPlayer(it.key) }
+        onlinePlayers.forEach { player, status ->
+            when (status[Users.vanishStatus]) {
+                true -> players.forEach {
+                    it.hidePlayer(LobbyMain.instance, player)
+                }
+                false -> when (status[Users.hiddenStatus]) {
+                    VisibilityStates.ALL -> {
+                        players.forEach { player.showPlayer(LobbyMain.instance, it) }
                     }
-                    false -> when (status[Users.hiddenStatus]) {
-                        VisibilityStates.ALL -> {
-                            players.forEach { player.showPlayer(LobbyMain.instance, it) }
-                        }
-                        VisibilityStates.NONE -> {
-                            players.forEach { player.hidePlayer(LobbyMain.instance, it) }
-                        }
-                        VisibilityStates.FRIENDS -> {
-                            players.forEach {
-                                when (Relationships.areFriends(player, it)) {
-                                    true -> player.showPlayer(LobbyMain.instance, it)
-                                    false -> player.hidePlayer(LobbyMain.instance, it)
-                                }
+                    VisibilityStates.NONE -> {
+                        players.forEach { player.hidePlayer(LobbyMain.instance, it) }
+                    }
+                    VisibilityStates.FRIENDS -> {
+                        players.forEach {
+                            when (Relationships.areFriends(player, it)) {
+                                true -> player.showPlayer(LobbyMain.instance, it)
+                                false -> player.hidePlayer(LobbyMain.instance, it)
                             }
                         }
                     }
